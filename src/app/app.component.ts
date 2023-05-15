a4f368f7927c25f15ad34511103494c3a29f5580
infinitee404 <anudeet.74@gmail.com>
2023-05-15 08:51:39 +0545

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-name';
+  user: User = {
+    name: "Change",
+    surname: "Me"
+  };
+
+  // submit() {
+  //   alert(`Submited with: ${this.user.name} ${this.user.surname}`);
+  // }
+
+  name: string = 'Tomas';
+
+  // it is possible to react to the input value change 
+  // (which is not the case when using ngModel)
+  handleNameChange(name: string) {
+    this.name = name;
+  }
+
+  submit() {
+    alert(`Submited with: ${this.name}`);
+  }
 }
+
+export interface User {
+  name: string;
+  surname: string;
+}
+
